bc6118ff504bd9d06f6d480bae19517d6b61c433
Ben Chitty <htmlgxn@protonmail.com>
2021-05-07 17:51:01 -0300

    Made three comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import profile1 from './img/profile1.jpg'
+import profile2 from './img/profile2.jpg'
+import profile3 from './img/profile3.jpg'
 
 const App = () => {
     return(
-        <div>
-            Hello World!
+        <div className='ui comments'>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src={profile1} alt="profile picture" />
+                </a>
+                <div className='content'>
+                    <a href="/" className='author'>
+                        Sarah
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>
+                            Today at 5:00PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's amazing!
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src={profile2} alt="profile picture" />
+                </a>
+                <div className='content'>
+                    <a href="/" className='author'>
+                        Bob
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>
+                            Today at 6:00PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's amazing!
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src={profile3} alt="profile picture" />
+                </a>
+                <div className='content'>
+                    <a href="/" className='author'>
+                        Yakob
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>
+                            Today at 7:00PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's amazing!
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
